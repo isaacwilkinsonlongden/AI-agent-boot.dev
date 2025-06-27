9070041d2f226c9848acb192cdf048a17af96cd7
Isaac Wilkinson-Longden <isaacwilkinsonlongden@gmail.com>
2025-06-27 14:15:27 +0100

    full conversation now stored in list

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
-import os
 import sys
-from dotenv import load_dotenv
+import os
 from google import genai
-
-if len(sys.argv) < 2:
-    print("Usage: python main.py <prompt>")
-    sys.exit(1)
+from google.genai import types
+from dotenv import load_dotenv
 
 
 def main():
     load_dotenv()
 
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model = "gemini-2.0-flash-001",
-        contents = sys.argv[1]
-    )
+    user_prompt = " ".join(args)
 
-    print("response:")
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    print("Response:")
     print(response.text)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":

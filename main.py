5baab788391ed48733f782e290fe33fdb77676d7
Isaac Wilkinson-Longden <isaacwilkinsonlongden@gmail.com>
2025-06-27 13:51:15 +0100

    added contents for response to be input through command line argument

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+
+if len(sys.argv) < 2:
+    print("Usage: python main.py <prompt>")
+    sys.exit(1)
 
 
 def main():
@@ -10,8 +15,8 @@ def main():
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        model = "gemini-2.0-flash-001",
+        contents = sys.argv[1]
     )
 
     print("response:")
